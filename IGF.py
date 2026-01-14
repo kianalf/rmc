4e2303a8bd697353f87f9a9fd11e07c0f3707f17
Kiana Fong <95658620+kianalf@users.noreply.github.com>
2026-01-14 08:57:32 -0500

    Add files via upload

--- IGF.py
+++ IGF.py
@@ -1,1 +1,9 @@
 def run_IGF():
+    import streamlit as st
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    from io import StringIO
+
+    st.set_page_config(layout="centered")
+    st.title("I (mA) vs Time (s) — Multi-File Plotter")
+    print ("igf")
