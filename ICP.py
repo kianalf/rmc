2aabaec8bc78b3c15c066ebfe1392e7bd0674b4f
Kiana Fong <95658620+kianalf@users.noreply.github.com>
2026-01-14 08:56:22 -0500

    Add files via upload

---
+++ ICP.py
@@ -0,0 +1,2 @@
+def run_ICP():
+    print ("icp")
--- root.py
+++ root.py
@@ -1,7 +1,8 @@
 import streamlit as st
 from H2 import run_H2
-#from code2 import run_code2
-#from code3 import run_code3
+from IGF import run_IGF
+from ICP import run_ICP
+from PDP import run_PDP
 
 st.set_page_config(layout="wide")
 
@@ -9,15 +10,17 @@ st.sidebar.title("Select Tool")
 
 choice = st.sidebar.radio(
     "Go to:",
-    ["H2 - I v. A", "Code 2", "Code 3"]
+    ["H2", "IGF", "ICP"]
 )
 
-if choice == "H2 - I v. A":
+if choice == "H2":
     run_H2()
 
-#elif choice == "Code 2":
-   # run_code2()
+elif choice == "IGF":
+    run_IGF()
 
-#elif choice == "Code 3":
-    #run_code3()
+elif choice == "ICP":
+    run_ICP()
 #
+elif choice == "PDP":
+    run_PDP()
